70dcfc702a03022b12e3f748322cfbd400124a33
blue-script <alexm92@inbox.ru>
2025-05-12 18:33:50 +0500

    add Poetry, Pydantic V2, APIRouter, CRUD

---
+++ README.md
@@ -0,0 +1,2 @@
+dev run project 
+`python main.py`
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
 import uvicorn
+from items_views import router as items_router
+from users.views import router as users_router 
 
 app = FastAPI()
-
-
-class CreateUser(BaseModel):
-    email: EmailStr
+app.include_router(items_router)
+app.include_router(users_router)
 
 
 @app.get("/")
@@ -20,29 +19,9 @@ def hello(name: str = "World"):
     return {"message": f"Hello {name}"}
 
 
-@app.post("/users/")
-def create_user(user: CreateUser):
-    return {"message": "success", "email": user.email}
-
-
 @app.post("/calc/add/")
 def add(a: int, b: int):
     return {"a": a, "b": b, "result": a + b}
-
-
-@app.get("/items/")
-def list_items():
-    return ["Item1", "Item2"]
-
-
-@app.get("/items/latest/")
-def get_latest_item():
-    return {"item": {"id": "0", "name": "latest"}}
-
-
-@app.get("/items/{item_id}/")
-def get_item_by_id(item_id: int):
-    return {"item": {"id": item_id}}
 
 
 if __name__ == "__main__":
