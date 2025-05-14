323bde509ced400741422280a665ae6d48aa55b5
blue-script <alexm92@inbox.ru>
2025-05-14 16:16:39 +0500

    add black formatter and apply

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
 from items_views import router as items_router
-from users.views import router as users_router 
+from users.views import router as users_router
 
 app = FastAPI()
 app.include_router(items_router)
